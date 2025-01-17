d16bdac63327236ed58997011d9c81395458d49d
hmeyd <yyahmedsalm@gmail.com>
2025-01-18 00:35:15 +0100

    j ai changé la version de python

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end="")
+    if number < 0:
+        number = -number
+    last_digit = number % 10
+    print("{:d}".format(last_digit), end='')
     return (last_digit)
