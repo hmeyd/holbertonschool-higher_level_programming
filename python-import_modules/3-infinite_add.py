8852d01115edc386a5ec861bebbb4ba3015e398d
hmeyd <yyahmedsalm@gmail.com>
2025-01-18 01:08:02 +0100

    modif

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
-    result = sum(int(arg) for arg in sys.argv[1:])
-    print(result)
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
