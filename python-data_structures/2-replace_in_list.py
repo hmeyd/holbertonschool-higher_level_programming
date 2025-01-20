3cfd519672f73cf80904e3c32db7b0daa727c3e5
hmeyd <yyahmedsalm@example.com>
2025-01-20 12:03:33 +0100

    traiter le negative et out range

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,8 @@ def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
         return my_list
-    else:
+    elif idx < 0:
         return my_list
-    
+    elif idx > len(my_list):
+        return my_list 
+       
