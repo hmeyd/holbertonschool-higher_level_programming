43e9fd6ae9199dd4c40ce38291a974f2ed596ba4
hmeyd <yyahmedsalm@example.com>
2025-01-20 14:43:12 +0100

    modification de chaine

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
+    if not sentence:
         return 0, None
-    else:
-        return len(sentence), sentence[0]
+    return len(sentence), sentence[0]
