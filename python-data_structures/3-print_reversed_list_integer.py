7255ea1eda8ebc5bf78f74e2e4a2872a41848b3e
hmeyd <yyahmedsalm@example.com>
2025-01-20 12:23:54 +0100

    utilisation de str.format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list) - 1
     while i >= 0:
-        print(my_list[i])
+        print("{:d}".format(my_list[i]))
         i -= 1
-
-
-my_list = [1, 2, 7, 4, 10]
-print_reversed_list_integer(my_list)
