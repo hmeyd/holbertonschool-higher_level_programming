bdb1766f4721d62550b06ce1d47ec1857500f84c
hmeyd <yyahmedsalm@example.com>
2025-01-20 17:55:14 +0100

    ajout de tasck11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,8 +2,6 @@
 def delete_at(my_list=[], idx=0):
     new_list = []
     for i in range(len(my_list)):
-        if idx < 0 or idx > len(my_list):
-            return my_list
-        elif i != idx:
+        if i != idx:
             new_list.append(my_list[i])
-        return new_list
+    return new_list
