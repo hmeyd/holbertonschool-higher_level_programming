f3fb5f19163b5d589dc290dc9eda7f36a2fe04a6
hmeyd <yyahmedsalm@example.com>
2025-01-20 17:07:06 +0100

    rectufication de sortie

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         new_list = my_list[:]
         if my_list[i] % 2 == 0:
             new_list[i] = True
