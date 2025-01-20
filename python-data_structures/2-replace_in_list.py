f06d674083b4e82c717732a4b0ea294f5fa9dac7
hmeyd <yyahmedsalm@example.com>
2025-01-20 12:05:58 +0100

    refaire le code

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,8 +3,5 @@ def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list[idx] = element
         return my_list
-    elif idx < 0:
+    else:
         return my_list
-    elif idx > len(my_list):
-        return my_list 
-       
