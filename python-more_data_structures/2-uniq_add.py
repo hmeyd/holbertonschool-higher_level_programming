dcfefa77a08aecac1f7cabcc2145550bb426b8f1
hmeyd <yyahmedsalm@example.com>
2025-01-21 13:50:53 +0100

    suppression de range

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    for i in range(len(my_list)):
+    for i in my_list:
         k = 1
         for j in range(i):
             if my_list[j] == my_list[i]:
