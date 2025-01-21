3a28f38fa8d2f04ce347e469b17f3f9a86295e0b
hmeyd <yyahmedsalm@example.com>
2025-01-21 23:50:21 +0100

    ajout de condition Null ou n est pas str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     sum = 0
     R_list ={"I" : 1, "V" : 5, "X" : 10, "L": 50, "C" : 100, "D" : 500, "M" : 1000}
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     for i in range(len(roman_string)):
         if i < len(roman_string) - 1:
             if roman_string[i] == "I" and (roman_string[i + 1] == "V" or roman_string[i + 1]  == "X"):
@@ -13,21 +15,6 @@ def roman_to_int(roman_string):
             sum = sum + R_list[roman_string[i]]
     return(sum)
     
-
-roman_number = "IV"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 
 
