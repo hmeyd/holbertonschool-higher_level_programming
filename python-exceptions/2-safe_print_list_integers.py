ab0e2a84a22a8ea8983c90ae30fb7074cc464daa
hmeyd <yyahmedsalm@gmail.com>
2025-01-22 14:20:36 +0100

    suppression de IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,26 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
     return count
-
-
-<<<<<<< HEAD
-
-my_list = [1, 2, 3, 4, 5]
-=======
-# Code principal
-my_list = [1, 2, 3, 4, 5]
-
->>>>>>> 8abf48fac7a5565f35cb56ebffbcbf37d5a6a445
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
