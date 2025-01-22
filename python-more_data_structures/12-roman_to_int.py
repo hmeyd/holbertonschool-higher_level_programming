9b03a741fc95c7ecad4a1bbc1a6df0daca91bb8c
hmeyd <yyahmedsalm@example.com>
2025-01-22 01:30:03 +0100

    façon de traiter

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    
     sum = 0
     R_list = {
         "I": 1, "V": 5, "X": 10, "L": 50,
         "C": 100, "D": 500, "M": 1000
     }
 
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-
     for i in range(len(roman_string)):
         if i < len(roman_string) - 1:
-            if (roman_string[i] == "I" and
-               (roman_string[i + 1] == "V" or roman_string[i + 1] == "X")):
-                sum = sum + R_list[roman_string[i]] - 2
+            if R_list[roman_string[i]] < R_list[roman_string[i + 1]]:
+                sum -= R_list[roman_string[i]]
             else:
-                sum = sum + R_list[roman_string[i]]
+                sum += R_list[roman_string[i]]
         else:
-            sum = sum + R_list[roman_string[i]]
+            sum += R_list[roman_string[i]]
 
     return sum
