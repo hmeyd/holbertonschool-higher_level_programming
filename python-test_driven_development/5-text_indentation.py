0d0fa58ae3df9f6d46132aba6eb1a48c20701d3b
hmeyd <yyahmedsalm@gmail.com>
2025-01-24 22:25:33 +0100

    modif

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,11 +6,10 @@ avec 2 nouvelles lignes après chacun de ces caractères : ., ? et :
 Il ne doit y avoir aucun espace au début ou à la fin de chaque ligne imprimée.
 """
 
-
 def text_indentation(text):
     """
     Affiche le texte avec 2 nouvelles lignes après
-    chaque occurrence de '.', '?', ou ':'.
+    chaque occurrence de '.', '?', ou ':'. 
 
     Arguments:
     text (str): Le texte à afficher.
@@ -20,13 +19,21 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    
     delimiters = ['.', '?', ':']
     new_text = ""
-
+    skip_spaces = True
+    
     for char in text:
+        if skip_spaces and char == ' ':
+            continue
+        skip_spaces = False
+        
         new_text += char
         if char in delimiters:
-            print(new_text.strip())
+            print(new_text, end="\n\n")
             new_text = ""
+            skip_spaces = True
+    
     if new_text:
-        print(new_text.strip())
+        print(new_text)
