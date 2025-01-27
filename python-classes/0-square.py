5b22ffae4e93d17c5e135586a4ac5aec8872ac6e
hmeyd <yyahmedsalm@gmail.com>
2025-01-27 03:06:59 +0100

    ajout de commentaire

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Ce module fournit une classe Square pour représenter des carrés.
+La classe Square permet de manipuler et de calculer des propriétés des carrés.
+"""
+
+
 class Square:
+
+    """
+    Une classe qui définit un carré.
+
+    Attributs :
+        Aucun pour le moment (c'est un exemple minimal).
+    """
     pass
