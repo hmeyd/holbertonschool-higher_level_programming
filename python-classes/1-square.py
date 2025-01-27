b0bb01f23dc28e2861d16bd334379b7f9bb621a4
hmeyd <yyahmedsalm@gmail.com>
2025-01-27 03:14:17 +0100

    ajout de task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Ce module fournit une classe Square pour représenter des carrés.
+La classe Square permet de manipuler et de calculer des propriétés des carrés.
+"""
+
+
+class Square:
+
+    """
+    Une classe qui définit un carré.
+
+    Attributs :
+        oject has non attribute 'size'.
+    """
+    def __init__(self, height=0, width=0):
+        self.height = height
+        self.width = width
