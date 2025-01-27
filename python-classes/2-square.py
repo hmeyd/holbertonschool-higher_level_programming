68ab33597ea637910cda0151c94d4caca5d3b191
hmeyd <yyahmedsalm@gmail.com>
2025-01-27 13:17:16 +0100

    pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,11 +14,10 @@ class Square:
         oject has non attribute 'size'.
     """
     def __init__(self, size=0):
-        
+
         self.__size = size
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
