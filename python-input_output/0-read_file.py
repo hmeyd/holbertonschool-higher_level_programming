cc57c10246086ea7f1bfd2094ebddcf4e6658616
hmeyd <yyahmedsalm@gmail.com>
2025-02-10 01:31:34 +0100

    commentaires

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+Module that contains the function read_file.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads a UTF-8 text file and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to be read.
+    Default is an empty string.
+
+    Returns:
+        None
+    """
     with open(filename, "r") as f:
         for line in f:
             print(line.strip)
