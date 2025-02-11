5b16e06a2abbc5cdddc8888ce03c5593b1a50d37
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 12:38:14 +0100

    pycodestyle

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,22 +4,13 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-    
+
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
     pass
+
 
 def load_and_deserialize(filename):
     with open(filename, "r", encoding="utf-8") as f:
         return json.__dict__
     pass
-
-# Sample data to be serialized
-data_to_serialize = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York"
-}
-
-# Serialize the data to JSON and save it to a file
-serialize_and_save_to_file(data_to_serialize, 'data.json')
