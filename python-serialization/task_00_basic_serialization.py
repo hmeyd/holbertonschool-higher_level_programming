34217f11c64f8c6552db76a51163c83ac4cca309
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 12:43:58 +0100

    enlever le mot __dict__

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,12 +6,10 @@ import json
 def serialize_and_save_to_file(data, filename):
     """to serialize and save data to the specified file"""
     with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
-    pass
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     """to load and deserialize data from the specified file"""
     with open(filename, "r") as f:
-        return json.__dict__
-    pass
+        return json.load(f)
