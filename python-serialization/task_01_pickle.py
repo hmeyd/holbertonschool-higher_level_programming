00ad0373fdaf3fa09bce6f6c52cd584268e90f38
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 13:06:22 +0100

    serialise

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -9,15 +9,18 @@ import pickle
 class CustomObject:
     """This class should have the following attributes"""
     def __init__(self, first_name, last_name, age):
+        """fuction qui initialise le valeur"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def serialize(self, filename):
+        """serialize"""
         with open(filename, "wb") as f:
             pickle.dump(self, f)
 
     @classmethod
     def deserialize(cls, filename):
+        """deserialize"""
         with open(filename, "rb") as f:
             return pickle.load(f)
