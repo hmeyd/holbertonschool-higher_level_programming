8945b611dcce33496a87a76a53f2d96449474a1f
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 12:40:59 +0100

    enlever le module

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,13 +5,13 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     """to serialize and save data to the specified file"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         json.dump(data, f, indent=4)
     pass
 
 
 def load_and_deserialize(filename):
     """to load and deserialize data from the specified file"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r") as f:
         return json.__dict__
     pass
