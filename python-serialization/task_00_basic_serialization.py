324106c2fd14f576c3aac007f14fc481107f82da
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 12:40:03 +0100

    documentation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,13 +4,14 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-
+    """to serialize and save data to the specified file"""
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
     pass
 
 
 def load_and_deserialize(filename):
+    """to load and deserialize data from the specified file"""
     with open(filename, "r", encoding="utf-8") as f:
         return json.__dict__
     pass
