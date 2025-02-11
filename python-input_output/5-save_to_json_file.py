7f55cd9da9fc8b836bf70116f59f3adac2dabaf7
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 01:24:55 +0100

    commit

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ function """
     with open(filename, "w", encoding="utf-8") as f:
         jason = json.dumps(my_obj)
         return f.write(jason)
