518f15b7cb41f8b7aa751197b4a5c3dfd5515349
hmeyd <yyahmedsalm@gmail.com>
2025-02-11 12:26:05 +0100

    ajout de passe

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,5 +10,6 @@ def serialize_and_save_to_file(data, filename):
     pass
 
 def load_and_deserialize(filename):
-     with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.__dict__
+    pass
