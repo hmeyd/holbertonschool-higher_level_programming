9af558182a6098474e97b86c2b3e95bd6d124bf0
hmeyd <yyahmedsalm@gmail.com>
2025-02-18 21:59:45 +0100

    task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
 """
-a basic Python script to fetch posts from
-JSONPlaceholder using requests
+Un script Python basique pour récupérer des posts de
+JSONPlaceholder en utilisant requests
 """
-
 
 import requests
 import csv
 
 
 def fetch_and_print_posts():
-    """Fetch posts and print their titles"""
+    """Récupérer les posts et imprimer leurs titres"""
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print("status_code:{}".format(response.status_code))
+    print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post["title"])
     else:
-        print("failed")
+        print("Échec de la récupération des posts")
 
 
 def fetch_and_save_posts():
-
-    """Fetch posts and save them into a CSV file"""
+    """Récupérer les posts et les enregistrer dans un fichier CSV"""
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
+    print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
         posts = response.json()
         fieldnames = ["id", "title", "body"]
-        with open('posts.csv', mode="w", encoding="utf-8", newline="") as csvfile:
+        with open('posts.csv', mode="w", encoding="utf-8",
+                  newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for post in posts:
                 writer.writerow({"id": post["id"], "title":
                                  post["title"], "body": post["body"]})
-        print("posts have been saved")
+        print("Les posts ont été enregistrés dans 'posts.csv'")
     else:
-        print("failed")
+        print("Échec de la récupération des posts")
