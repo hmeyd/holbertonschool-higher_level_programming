48e0d92abb5b8a985be1f73a8260949da4496168
hmeyd <yyahmedsalm@gmail.com>
2025-02-18 23:51:32 +0100

    task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Develop a simple API using Python with the `http.server` module
+"""
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    """Handler to process GET requests."""
+    def do_GET(self):
+        response_data = {}
+        if self.path == '/data':
+            response_data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(json.dumps(response_data).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not Found")
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler):
+    """Run the HTTP server."""
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Server running on http://localhost:8000...")
+    httpd.serve_forever()
