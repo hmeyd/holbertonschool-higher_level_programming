33ada1116542520e9521de422de64e1acd8b4cd5
hmeyd <yyahmedsalm@gmail.com>
2025-02-18 21:49:15 +0100

    pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,6 +7,8 @@ JSONPlaceholder using requests
 
 import requests
 import csv
+
+
 def fetch_and_print_posts():
     """Fetch posts and print their titles"""
     url = "https://jsonplaceholder.typicode.com/posts"
@@ -19,17 +21,20 @@ def fetch_and_print_posts():
     else:
         print("failed")
 
+
 def fetch_and_save_posts():
+
     """Fetch posts and save them into a CSV file"""
     url = "https://jsonplaceholder.typicode.com/posts"
     reponse = requests.get(url)
     if reponse.status_code == 200:
         posts = reponse.json()
         fieldnames = ["id", "title", "body"]
-        with open('posts.csv', mode="w",encoding= "utf-8") as csvfile:
-            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        with open('posts.csv', mode="w", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             for post in posts:
-                writer.writerow({"id": post["id"],"title": post["title"], "body": post["body"]})
+                writer.writerow({"id": post["id"], "title":
+                                 post["title"], "body": post["body"]})
         print("posts have been saved")
     else:
         print("failed")
