aa25257cfb7eee59046e93d3d69feda8faafc8ed
hmeyd <yyahmedsalm@gmail.com>
2025-03-05 21:52:38 +0100

    modif

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
--- script that prints the following description of a table
+-- Script that prints the structure of first_table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
