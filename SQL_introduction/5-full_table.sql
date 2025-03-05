580247b12f567d42dfb81761d395e0001e5b34df
hmeyd <yyahmedsalm@gmail.com>
2025-03-05 21:49:15 +0100

    correction of TABLE_SCHEMA

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- script that prints the following description of a table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
