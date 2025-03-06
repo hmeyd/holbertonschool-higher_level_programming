79927f91d10b08390b4c22fcd79e928dda772e80
hmeyd <yyahmedsalm@gmail.com>
2025-03-06 01:36:30 +0100

    modif

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Création de l'utilisateur 'user_0d_1' s'il n'existe pas déjà
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
