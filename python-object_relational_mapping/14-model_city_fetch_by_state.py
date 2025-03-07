1be94641a7a278001b691568cf858defb9631329
hmeyd <yyahmedsalm@example.com>
2025-03-07 16:30:05 +0100

    recti

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     cities = session.query(City).order_by(City.id).all()
 
-    for city, state in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
 
     session.close()
