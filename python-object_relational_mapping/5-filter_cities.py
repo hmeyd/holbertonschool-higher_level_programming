50a1150d5e590ab0715ea4bb76418ed965a4ae73
hmeyd <yyahmedsalm@example.com>
2025-03-07 16:27:14 +0100

    task 7

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
-"""
-This module lists all cities from the database hbtn_0e_4_usa
-"""
+"""Module that query USA Cities from a database table"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    state = sys.argv[4]
+    cur.execute("SELECT cities.name \
+        FROM cities \
+        LEFT JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC", (state,))
+    query_rows = cur.fetchall()
 
-    # Connexion à la base de données avec charset="utf8"
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=mysql_user, passwd=mysql_password,
-                         db=database_name, charset="utf8")
+    first = True
+    for row in query_rows:
+        for city in row:
+            if not first:
+                print(", ", end='')
+            print(city, end="")
+            first = False
+    print()
 
-    cursor = db.cursor()
-
-    # Requête SQL
-    query = """
-    SELECT cities.name
-    FROM cities
-    LEFT JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC;
-    """
-
-    cursor.execute(query, (state_name,))
-    cities = cursor.fetchall()
-
-    # Affichage des villes
-    if cities:
-        print(", ".join(city[0] for city in cities))
-
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
