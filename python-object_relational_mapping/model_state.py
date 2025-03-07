075ce972f0227e33a9706426553fcde88547e4e2
hmeyd <yyahmedsalm@example.com>
2025-03-07 14:24:46 +0100

    mode

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,20 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """This class defines states table"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    def __repr__(self):
-        return f"<State(id={self.id}, name={self.name})>"
-
-engine = create_engine('mysql+mysqldb://<mysql_user>:'
-'<mysql_password>@localhost:3306/<database_name>')
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-new_state = State(name="California")
-session.add(new_state)
-session.commit()
-
-session.close()
