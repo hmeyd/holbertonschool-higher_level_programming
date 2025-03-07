12749ad8f7c2b0329f2f47c6dacc3842781cb6f7
hmeyd <yyahmedsalm@example.com>
2025-03-07 16:26:13 +0100

    push

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,22 +11,29 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name = sys.argv[4]
+
+    # Connexion à la base de données avec charset="utf8"
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=mysql_user, passwd=mysql_password,
-                         db=database_name)
+                         db=database_name, charset="utf8")
 
     cursor = db.cursor()
 
+    # Requête SQL
     query = """
     SELECT cities.name
     FROM cities
-    INNER JOIN states ON cities.state_id = states.id
+    LEFT JOIN states ON cities.state_id = states.id
     WHERE states.name = %s
-    ORDER BY cities.id ASC
+    ORDER BY cities.id ASC;
     """
+
     cursor.execute(query, (state_name,))
     cities = cursor.fetchall()
+
+    # Affichage des villes
     if cities:
         print(", ".join(city[0] for city in cities))
+
     cursor.close()
     db.close()
