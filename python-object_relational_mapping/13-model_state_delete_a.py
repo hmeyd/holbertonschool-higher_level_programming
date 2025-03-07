dfac016122d569c901ef9aef18bdddd34fb23234
hmeyd <yyahmedsalm@example.com>
2025-03-07 14:41:33 +0100

    push

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""Delete all State objects with a name containing
+the letter 'a'"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -12,14 +13,14 @@ if __name__ == "__main__":
                                    sys.argv[2],
                                    sys.argv[3]),
                            pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(engine)
     session = Session()
 
     states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
         session.delete(state)
-
     session.commit()
+
     session.close()
